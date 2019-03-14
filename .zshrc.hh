f69dace81d053aa8eb93027e144efa52a3bbaf9d
Hanlin He <hanlhe@tetrationanalytics.com>
2019-03-14 12:41:34 -0700

    Add ssh cluster short cut.

--- .zshrc.hh
+++ .zshrc.hh
@@ -9,3 +9,4 @@ changelaunchpadlayout() {
     defaults write com.apple.dock springboard-columns -int $1;defaults write com.apple.dock springboard-rows -int $2;defaults write com.apple.dock ResetLaunchPad -bool TRUE;killall Dock
 }
 alias cll=changelaunchpadlayout
+alias git=hub
